2d5936c495c5514ebc4ba82f8e25620302d064e1
Kangxin Liu <kl3574@nyu.edu>
2021-08-27 13:48:47 -0400

    print second moment; optimize yaml file

--- data/random_walk/random_walk.py
+++ data/random_walk/random_walk.py
@@ -1,5 +1,6 @@
 import numpy as np
 nlcc_list = np.array(random_walk(100,1.0,3))
-print(nlcc_list)
+print("trj:",nlcc_list)
 second_moment = np.sum(nlcc_list*nlcc_list)
+print("second moment:",second_moment)
 result = True if second_moment == 100*(1.0**2)*3 else False
