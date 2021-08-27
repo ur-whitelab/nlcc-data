dbce41de1a12a4ce5a880d4a94a0e3e4a4362d5b
Kangxin Liu <kl3574@nyu.edu>
2021-08-27 14:02:16 -0400

    correct evaluation; optimize yaml file (label is proved useless)

--- data/random_walk/random_walk.py
+++ data/random_walk/random_walk.py
@@ -1,6 +1,7 @@
 import numpy as np
 nlcc_list = np.array(random_walk(100,1.0,3))
-print("trj:",nlcc_list)
-second_moment = np.sum(nlcc_list*nlcc_list)
+#print("trj:",nlcc_list)
+second_moment = np.sum(nlcc_list[-1]*nlcc_list[-1])
+ideal = 100*(1.0**2)*3
 print("second moment:",second_moment)
-result = True if second_moment == 100*(1.0**2)*3 else False
+result = True if np.abs((second_moment-ideal)/ideal) < 0.01 else False
