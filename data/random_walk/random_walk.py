30fcc0be0e475928fd1eb0d548305f4d053b4f31
Kangxin Liu <kl3574@nyu.edu>
2021-08-30 16:48:19 -0400

    change criterion

--- data/random_walk/random_walk.py
+++ data/random_walk/random_walk.py
@@ -2,14 +2,17 @@ import numpy as np
 # run 100 times
 nlcc_trj_list = []
 for i in range(100):
-    nlcc_trj = random_walk(20000,1.0,2)
+    nlcc_trj = random_walk(10000,1.0,2)
     nlcc_trj_list.append(nlcc_trj)
 nlcc_trj_list = np.array(nlcc_trj_list)
 print("random walk shape:",nlcc_trj_list.shape)
 second_moment = np.sum(nlcc_trj_list*nlcc_trj_list,axis=-1)
-#a,b = np.polyfit(np.arange(20000)[10000:],second_moment[10000:],1)
-#print("a =",a,", b =",b)
-a = np.mean(second_moment[:,10000:] / np.arange(20000)[10000:])
+a_list = []
+for i in range(100):
+    a,b = np.polyfit(np.arange(10000),second_moment[i],1)
+    a_list.append(a)
+a = np.mean(np.array(a_list))
+#a = np.mean(second_moment[:,10000:] / np.arange(20000)[10000:])
 print("a =",a)
 ideal = 2.0
 result = True if np.abs((a-ideal)/ideal) < 0.1 else False
