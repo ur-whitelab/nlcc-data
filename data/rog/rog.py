02ca0050374c15eac544a8737eaac55080b55c2e
Glen Hocky <hockyg@nyu.edu>
2021-08-30 16:01:12 -0400

    corrected test rog code and check criterea, and file loading. changed function name to make more difficult

--- data/rog/rog.py
+++ data/rog/rog.py
@@ -1,19 +1,20 @@
 import numpy as np
-import mdtraj as md
 import sys
+import os
 
-pdb_file=sys.argv[1]
+pdb_file = os.path.join(_FILE_DIR_,"1VII.pdb")
 
 def compute_rg_mdtraj(pdb_file):
+    import mdtraj as md
     pdb = md.load(pdb_file)
-    rg = md.compute_rg(pdb_file)
+    rg = md.compute_rg(pdb)
     return rg
 
 my_rg = compute_rg_mdtraj(pdb_file)
-nlcc_rg = compute_rg_nlcc(pdb_file)
+nlcc_rg = protein_radius_of_gyration(pdb_file)
 
-print("My result:", my_rg)
-print("nlcc result", nlcc_rg)
+#print("My result:", my_rg)
+#print("nlcc result", nlcc_rg)
 
-result = True if np.all( my_dipole == nl_dipole ) else False
+result = True if np.abs( my_rg - nlcc_rg )<0.1 else False
 
