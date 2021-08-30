f5e2ad0015284ef00acb96d8081e989b40237360
Kangxin Liu <kl3574@nyu.edu>
2021-08-30 15:42:44 -0400

    change py file

--- data/random_walk/random_walk.py
+++ data/random_walk/random_walk.py
@@ -1,7 +1,8 @@
 import numpy as np
-nlcc_list = np.array(random_walk(20000,2.0,3))
+nlcc_list = np.array(random_walk(20000,1.0,3))
+print("random walk shape:",nlcc_list.shape)
 second_moment = np.sum(nlcc_list*nlcc_list,axis=-1)
 a,b = np.polyfit(np.arange(20000)[10000:],second_moment[10000:],1)
-ideal = 3.0 * (2.0**2)
+ideal = 3.0
 print("a =",a,", b =",b)
 result = True if np.abs((a-ideal)/ideal) < 0.1 else False
