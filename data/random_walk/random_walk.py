65723481ae2aa0071ee301b62d15be4e9a6dfa09
Kangxin Liu <kl3574@nyu.edu>
2021-08-30 15:25:01 -0400

    correct py file

--- data/random_walk/random_walk.py
+++ data/random_walk/random_walk.py
@@ -1,7 +1,7 @@
 import numpy as np
 nlcc_list = np.array(random_walk(10000,2.0,3))
 second_moment = np.sum(nlcc_list*nlcc_list,axis=-1)
-a,b = np.polyfit(np.arange(10001),second_moment)
+a,b = np.polyfit(np.arange(10001),second_moment,1)
 ideal = 3.0 * (2.0**2)
 print("a =",a,", b =",b)
 result = True if np.abs((a-ideal)/ideal) < 0.01 else False
