e38f3246fa9167b6aa58a2375be6f8bdff881264
Heta Gandhi <hgandhi@ur.rochester.edu>
2021-08-30 11:41:32 -0700

    Taking Glen and Andrew's suggestions

--- data/pdb/pdb.py
+++ data/pdb/pdb.py
@@ -1,3 +1,4 @@
 from mdtraj import load_pdb
-t = load_pdb('G5.pdb')
-result = True if load_from_pdb('G5.pdb').n_atoms == t.n_atoms else False
+import os
+t = load_pdb(os.path.join(_FILE_DIR_,'G5.pdb'))
+result = True if load_from_pdb(os.path.join(_FILE_DIR_,'data/pdb/G5.pdb')).n_atoms == t.n_atoms else False
