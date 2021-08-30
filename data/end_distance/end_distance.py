6a4863185e6efd00cbfb73e8d99f6082431044df
Glen Hocky <hockyg@nyu.edu>
2021-08-30 16:28:14 -0400

    use special file dir name to find pdb file. correct typos

--- data/end_distance/end_distance.py
+++ data/end_distance/end_distance.py
@@ -1,14 +1,13 @@
+import os
 import mdtraj as md
 import numpy as np
-pdb = md.load_pdb("1l2y.pdb")
+pdb_file = os.path.join(_FILE_DIR_,"1l2y.pdb")
+pdb = md.load_pdb(pdb_file)
 top = pdb.topology
 CAs = [atom.index for atom in top.atoms if atom.name == 'CA']
-#atom_pairs = []
-#atom_pair = [CAs[0],CAs[-1]]
-#atom_pairs.append(atom_pair)
-#atom_pairs = np.array(atom_pairs)
-atoms_pairs = [[CAs[0],CAs[-1]]]
-distance = md.compute_distances(pdb,atom_pairs)
-distance_prompt = get_distance("1L2y.pdb")
-diff = abs(distance_prompt - distance[0,0])
+atom_pairs = np.array([ [CAs[0],CAs[-1]], ]).reshape(-1,2)
+
+distance = md.compute_distances(pdb,atom_pairs)[0][0]
+distance_prompt = get_distance(pdb_file)
+diff = abs(distance_prompt - distance)
 result = True if diff <= 0.01 else False
