152a664c2e91b09ddb66cebedfd66f6bafd00af8
Kangxin Liu <kl3574@nyu.edu>
2021-08-30 16:35:15 -0400

    correct py file

--- data/random_walk/random_walk.py
+++ data/random_walk/random_walk.py
@@ -9,7 +9,7 @@ print("random walk shape:",nlcc_trj_list.shape)
 second_moment = np.sum(nlcc_trj_list*nlcc_trj_list,axis=-1)
 #a,b = np.polyfit(np.arange(20000)[10000:],second_moment[10000:],1)
 #print("a =",a,", b =",b)
-a = np.mean(second_moment[10000:] / np.arange(20000)[10000:])
+a = np.mean(second_moment[:,10000:] / np.arange(20000)[10000:])
 print("a =",a)
 ideal = 2.0
 result = True if np.abs((a-ideal)/ideal) < 0.1 else False
