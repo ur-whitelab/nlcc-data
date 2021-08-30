d817e005b71207bc42915453369c4fbcb317a196
Yuvraj Singh <ys3382@nyu.edu>
2021-08-30 17:37:32 -0400

    generate a list of numbers

--- data/gen_list/gen_list.py
+++ data/gen_list/gen_list.py
@@ -1,6 +1,5 @@
 import numpy as np
 import sys
-from math import exp
 
 start = 300
 N = 40
@@ -9,7 +8,7 @@ end = 400
 def list_generator(start,N,end):
     list=[]
     for i in range(0,N):
-        T_k=np.round( start + (end-start) * (1 - exp( float(i)/(N-1) )**1)/(1- exp(1)),2)
+        T_k=np.round( start + (end-start) * (1 - np.exp( float(i)/(N-1) )**1)/(1- np.exp(1)),2)
         list.append(T_k)
     return np.array(list)
 
