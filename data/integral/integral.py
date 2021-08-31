104063faa063382bd1758f74757bcbcacf7ce2c3
Heta Gandhi <hgandhi@ur.rochester.edu>
2021-08-31 11:41:58 -0700

    Fixed typo and integral test coefficients

--- data/integral/integral.py
+++ data/integral/integral.py
@@ -1,3 +1,3 @@
 import numpy as np
 
-result = True if find_integral(1,2,3,4) == np.poly1d([1/4, 2/3, 3/2, 4]) else False 
+result = True if find_integral(1,2,3,4) == np.poly1d([1/4, 2/3, 3/2, 4, 0]) else False 
