375b4c38c1ede6b705607f8c96eacb9557720c75
geemi725 <gwellawa@ur.rochester.edu>
2021-08-31 16:30:54 -0400

    path to files added

--- data/sel-frames/select-frames.py
+++ data/sel-frames/select-frames.py
@@ -2,4 +2,4 @@ import os
 import mdtraj as md 
 
 traj = md.load(os.path.join(_FILE_DIR_,'trp-cage-small.trr'), top=os.path.join(_FILE_DIR_,'trp-cage.pdb'))
-result = True if select-frames('trp-cage-small.trr','trp-cage.pdb',50).n_frames == traj[:50].n_frames else False
+result = True if select-frames(os.path.join(_FILE_DIR_,'trp-cage-small.trr'),os.path.join(_FILE_DIR_,'trp-cage.pdb'),50).n_frames == traj[:50].n_frames else False
