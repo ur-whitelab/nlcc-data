1c4910818a07d9e37f0cfb9edbc761259527e87c
geemi725 <gwellawa@ur.rochester.edu>
2021-08-31 16:27:15 -0400

    select first n frames from traj

--- data/rm-water/remove-water.py
+++
@@ -1,6 +0,0 @@
-import os
-import mdtraj as md 
-
-traj = md.load(os.path.join(_FILE_DIR_,'trp-cage-small.trr'), top=os.path.join(_FILE_DIR_,'trp-cage.pdb'))
-nowater = traj.remove_solvent()
-result = True if remove_water(os.path.join(_FILE_DIR_,'trp-cage-small.trr'),os.path.join(_FILE_DIR_,'trp-cage.pdb')).n_atoms == nowater.n_atoms else False 
