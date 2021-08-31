96a3f63612fb7da8eb63f44dee73df5d74e6bdb7
geemi725 <gwellawa@ur.rochester.edu>
2021-08-31 15:28:24 -0400

    revert to _file_dir

--- data/rm-water/remove-water.py
+++ data/rm-water/remove-water.py
@@ -3,5 +3,5 @@ import mdtraj as md
 
 traj = md.load('trp-cage-small.trr', top='trp-cage.pdb')
 nowater = traj.remove_solvent()
-result = True if remove_water(os.path.join(os.getcwd(),'trp-cage-small.trr'),os.path.join(os.getcwd(),'trp-cage.pdb')).n_atoms == nowater.n_atoms else False 
-#result = True if remove_water('trp-cage-small.trr','trp-cage.pdb').n_atoms == nowater.n_atoms else False
+result = True if remove_water(os.path.join(_FILE_DIR_,'trp-cage-small.trr'),os.path.join(_FILE_DIR_,'trp-cage.pdb')).n_atoms == nowater.n_atoms else False 
+
