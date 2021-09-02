3754a2a5068f7dce1f8302723c86b519df544845
Sam Cox <samantha.wright822@gmail.com>
2021-09-02 17:50:13 -0400

    updated invert_matrix files

--- data/matrix-factorial/Matrix_Factorial_att2.py
+++
@@ -1,9 +0,0 @@
-
-import numpy as np
-
-x = np.array([[1, 2, 3], [4,5,6]])
-m_f = matrix_factorial(x)
-x_f = np.array([[1, 2, 6], [24,120,720]])
-
-result = True if np.array_equal(x_f,m_f) == True else False
-
