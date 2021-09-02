04edf7f42d2bc6abe5e4b7cdc873aa59b7cefd88
sanjibpaul88 <89488057+sanjibpaul88@users.noreply.github.com>
2021-09-02 14:42:23 -0400

    Update end_distance.py

--- data/end_distance/end_distance.py
+++ data/end_distance/end_distance.py
@@ -1,13 +1,17 @@
 import os
-import mdtraj as md
+#import mdtraj as md
 import numpy as np
 pdb_file = os.path.join(_FILE_DIR_,"1l2y.pdb")
-pdb = md.load_pdb(pdb_file)
-top = pdb.topology
-CAs = [atom.index for atom in top.atoms if atom.name == 'CA']
-atom_pairs = np.array[ [CAs[0],CAs[-1]] ]#.reshape(-1,2)
+def compute_end_distance(pdf_file):
+  import mdtraj as md
+  pdb = md.load(pdf_file)
+  top = pdb.topology
+  CAs = [atom.index for atom in top.atoms if atom.name == 'CA']
+  atom_pairs = np.array[ [CAs[0],CAs[-1]] ]#.reshape(-1,2)
+  distance = md.compute_distances(pdb,atom_pairs)[0][0]
+  return distance
+my_distance = compute_end_distance(pdb_file)
+prompt_distance = get_distance(pdb_file)
 
-distance = md.compute_distances(pdb,atom_pairs)[0][0]
-distance_prompt = get_distance(pdb_file)
-diff = abs(distance_prompt - distance)
+diff = abs(prompt_distance - my_distance)
 result = True if diff <= 0.01 else False
