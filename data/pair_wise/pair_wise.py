e1d887d7aaa504f6a3c75ad9c26a67415feaa14b
Glen Hocky <hockyg@nyu.edu>
2021-09-02 14:52:14 -0400

    update pair wise check critereon to be the mean distance, since order of pairs could be different

--- data/pair_wise/pair_wise.py
+++ data/pair_wise/pair_wise.py
@@ -11,15 +11,13 @@ def pair_wise(pdb_file):
     cas=pdb.top.select("name CA")
     distances = []
     comb = combinations(cas, 2)
-    for p in comb:
-        d=md.compute_distances(pdb,[[p[0],p[1]]], periodic=True)
-        distances.append(d)
-    return np.array(distances).flatten()
+    distances = md.compute_distances(pdb,comb, periodic=True)
+    return distances
 
 my_result = pair_wise(pdb_file)
 nlcc_result = pair_wise_nlcc(pdb_file)
 
-print("My result:", my_result)
-print("nlcc result", nlcc_result)
+#print("My result:", my_result)
+#print("nlcc result", nlcc_result)
 
-result = True if np.all( my_result - nlcc_result )<0.1 else False
+result = True if np.abs(np.mean(my_result) - np.mean(nlcc_result) )<0.01 else False
