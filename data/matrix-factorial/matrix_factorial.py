f3a66b56ce0e0e0533f4b4474eba89093ba95567
Sam Cox <samantha.wright822@gmail.com>
2021-09-02 10:28:00 -0400

    modified matrix_factorial.py file

--- data/matrix-factorial/matrix_factorial.py
+++ data/matrix-factorial/matrix_factorial.py
@@ -2,9 +2,9 @@
 
 import numpy as np
 
-x = np.array([[1, 2, 3], [4, 5, 6]])
+x = np.array([[1, 2, 3], [24, 120, 720]])
 
-result = True if matrix_factorial(x) == array([[ 1,  2,  6],[ 2,  6, 24]]) else False
+result = True if matrix_factorial(x) == array([[ 1,  2,  6],[ 24, 120, 720]]) else False
 
 
 
