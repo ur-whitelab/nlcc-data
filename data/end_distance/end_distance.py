92f5ad5684c66f9fdc78fd360e9a184442771053
sanjibpaul88 <89488057+sanjibpaul88@users.noreply.github.com>
2021-09-02 14:31:17 -0400

    Update end_distance.py

--- data/end_distance/end_distance.py
+++ data/end_distance/end_distance.py
@@ -5,7 +5,7 @@ pdb_file = os.path.join(_FILE_DIR_,"1l2y.pdb")
 pdb = md.load_pdb(pdb_file)
 top = pdb.topology
 CAs = [atom.index for atom in top.atoms if atom.name == 'CA']
-atom_pairs = np.array([ [CAs[0],CAs[-1]] ])#.reshape(-1,2)
+atom_pairs = np.array[ [CAs[0],CAs[-1]] ]#.reshape(-1,2)
 
 distance = md.compute_distances(pdb,atom_pairs)[0][0]
 distance_prompt = get_distance(pdb_file)
