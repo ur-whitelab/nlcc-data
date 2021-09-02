d3f7c1b85fea249b93c708acdf3647a790fb3a37
Yuvraj Singh <45671754+yuvrajsingh24@users.noreply.github.com>
2021-09-02 15:02:28 -0400

    compute all pairwise residue distances of a protein (#30)
    * compute all pairwise residue distances of a protein
    
    * update pair wise check critereon to be the mean distance, since order of pairs could be different
    
    Co-authored-by: Glen Hocky <hockyg@nyu.edu>

---
+++ data/pair_wise/pair_wise.py
@@ -0,0 +1,23 @@
+import numpy as np
+import sys
+import os
+
+pdb_file = os.path.join(_FILE_DIR_,"1VII.pdb")
+
+def pair_wise(pdb_file):
+    import mdtraj as md
+    from itertools import combinations
+    pdb = md.load(pdb_file)
+    cas=pdb.top.select("name CA")
+    distances = []
+    comb = combinations(cas, 2)
+    distances = md.compute_distances(pdb,comb, periodic=True)
+    return distances
+
+my_result = pair_wise(pdb_file)
+nlcc_result = pair_wise_nlcc(pdb_file)
+
+#print("My result:", my_result)
+#print("nlcc result", nlcc_result)
+
+result = True if np.abs(np.mean(my_result) - np.mean(nlcc_result) )<0.01 else False
