e4c46410224fa54edc308f5638effd6c0ea03e22
Subarna Sasmal <69063658+SasmalSubarna@users.noreply.github.com>
2021-09-02 18:00:21 -0400

    Update partition_sho.py

--- data/partition_sho/partition_sho.py
+++ data/partition_sho/partition_sho.py
@@ -9,21 +9,21 @@ stretch_freq = 4342.0                       # in cm-1  (this one is for H2)
 c = 3.0e8                                   # speed of light(in m/s)
 omega = 2*np.pi*c*stretch_freq*100          # unit (in s-1) 
 k =  1.381e-23*na                           # boltzmann constant (j/mol.K)
-T =  300.0                                  # temperature in K 
+T =  3000.0                                  # temperature in K 
 h_bar = (6.634e-34*na)/(2*np.pi)            # J.s/mol
 beta = 1.0/(k*T)                            # (J/mol)-1
 
 
-print("=============INPUT PARAMS=========")
-print("avogadro's number =", na, "/mol")
-print("vibrational stretching frequency =", stretch_freq, "cm-1")
-print("speed of light =", c, "m/s")
-print("omega =", omega, "s-1")
-print("boltzmann constant =", k, "J/mol.K")
-print("temperature =", T, "K")
-print("planck's constant =", h_bar, "J.s/mol")
-print("beta (1/kT) =", beta, "(J/mol)-1")
-print("=================================")
+#print("=============INPUT PARAMS=========")
+#print("avogadro's number =", na, "/mol")
+#print("vibrational stretching frequency =", stretch_freq, "cm-1")
+#print("speed of light =", c, "m/s")
+#print("omega =", omega, "s-1")
+#print("boltzmann constant =", k, "J/mol.K")
+#print("temperature =", T, "K")
+#print("planck's constant =", h_bar, "J.s/mol")
+#print("beta (1/kT) =", beta, "(J/mol)-1")
+#print("=================================")
 
 
 def q_sho(b, h, w):
