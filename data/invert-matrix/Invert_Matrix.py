ba04ce3ddfbc4e5e01871e7c6b375af4f60b8bf8
Sam Cox <samantha.wright822@gmail.com>
2021-09-01 22:23:35 -0400

    Invert_Matrix 2nd Attempt Files

--- data/invert-matrix/Invert_Matrix.py
+++ data/invert-matrix/Invert_Matrix.py
@@ -5,7 +5,7 @@ import numpy as np
 x = np.array([[1,-1], [2,6]])
 
 print (x)
-result = True if Invert_Matrix(x) == array([[ 0.75,0.125],[-0.25,0.125]]) else False
+result = True if invert_matrix(x) == array([[ 0.75,0.125],[-0.25,0.125]]) else False
 
 
 
