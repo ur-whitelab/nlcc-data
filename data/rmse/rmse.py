a253f936c6323d552a42d3d5af3fe68904171caa
Mehrad Ansari <mehrad.ans@gmail.com>
2021-09-03 10:56:36 -0400

    updated rmse

--- data/rmse/rmse.py
+++ data/rmse/rmse.py
@@ -5,5 +5,5 @@ M = 100
 predictions = np.random.normal(loc=10, scale=3, size=(M,))
 targets = np.random.normal(loc=9, scale=2, size=(M,))
 
-rmse = np.sqrt(((predictions - targets) ** 2).mean())
-result = True if np.isclose(rmse, 3.61350) else False
+test_rmse = np.sqrt(((predictions - targets) ** 2).mean())
+result = True if np.isclose(rmse(predictions, test_rmse), 3.61350) else False
