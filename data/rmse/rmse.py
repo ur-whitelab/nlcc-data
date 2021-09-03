2487f94e341e76fe83193fb9746b40cc8dc1c645
Mehrad Ansari <mehrad.ans@gmail.com>
2021-09-03 10:58:44 -0400

    fixed typo

--- data/rmse/rmse.py
+++ data/rmse/rmse.py
@@ -6,4 +6,4 @@ predictions = np.random.normal(loc=10, scale=3, size=(M,))
 targets = np.random.normal(loc=9, scale=2, size=(M,))
 
 test_rmse = np.sqrt(((predictions - targets) ** 2).mean())
-result = True if np.isclose(rmse(predictions, test_rmse), 3.61350) else False
+result = True if np.isclose(rmse(predictions, targets), test_rmse) else False
