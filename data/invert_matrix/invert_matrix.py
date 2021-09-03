3b26852a137b4e3bd8dadc21fd36b5da6e15383c
Sam Cox <samantha.wright822@gmail.com>
2021-09-03 10:58:26 -0400

    changed py file and renamed

--- data/invert_matrix/invert_matrix.py
+++ data/invert_matrix/invert_matrix.py
@@ -6,8 +6,7 @@ x = np.array([[1,-1], [2,6]])
 m_i = invert_matrix(x)
 x_i = np.array([[0.75,0.125], [-0.25,0.125]])
 
-print (x)
-result = True if np.array_equal(m_i,x_i) == True else False
+result = np.array_equal(m_i,x_i)
 
 
 
