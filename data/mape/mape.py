cc0e0de6d30de9b6c19ac89a180ebb97774f079f
navneeth3005 <navneethgokul1991@gmail.com>
2021-09-03 10:45:45 -0400

    fix comparison in mape.py

--- data/mape/mape.py
+++ data/mape/mape.py
@@ -6,4 +6,4 @@ y_hat = np.random.normal(loc=10,scale=3,size=(M,))
 y = np.random.normal(loc=9, scale=2, size=(M,))
 
 test_mape = abs((y - y_hat)*100/ y).mean()
-result = True if mape(y_hat,y)==test_mape else False
+result = True if np.isclose(mape(y_hat,y),test_mape) else False
