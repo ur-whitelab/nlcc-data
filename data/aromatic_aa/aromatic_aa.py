2ae42616b6b76f4379b88581ea58936ea7fba6c0
Sam Cox <samantha.wright822@gmail.com>
2021-09-13 14:48:45 -0400

    added hydrogen donor and acceptor amino acid example

--- data/aromatic_aa/aromatic_aa.py
+++
@@ -1,7 +0,0 @@
-phenylalanine = "F"
-leucine = "L"
-
-
-result = True if aromatic_aa(phenylalanine)=="aromatic" and aromatic_aa(leucine) =="non-aromatic" and aromatic_aa("X") =="error" else False
-
-
