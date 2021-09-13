d0bc6cc0718ba5ae49f0fe5670c1388619de0a5d
Subarna Sasmal <ss12902@nyu.edu>
2021-09-13 12:19:34 -0400

    area of a triangle

---
+++ data/area_triangle/area.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+# sides of triangle
+a = 5.0
+b = 7.0
+c = 10.0
+
+# semi perimeter
+s = (a+b+c)/2
+
+area = np.sqrt(s*(s-a)*(s-b)*(s-c))
+print(area)
+
+area_codex= area_triangle(a,b,c)
+
+# check 
+if np.isclose(area, area_coedx) == True:
+    result = True
+else:
+    result = False
+
+
+
+
