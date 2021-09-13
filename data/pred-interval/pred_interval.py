4a1dfc364ff29d25fe84f938f3e2f9595591b7c7
geemi725 <gwellawa@ur.rochester.edu>
2021-09-13 15:18:28 -0400

    compute min num_trials needed to achive pred_interval

---
+++ data/pred-interval/pred_interval.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+prediction_level = 0.9
+samples = range(1, 100)
+p = 0.1
+psum = 0
+for n in samples:
+    psum += (1 - p) ** (n - 1) * p
+    if(psum >= prediction_level):
+        break
+
+result = True if num_trials(prediction_level)== n else false
