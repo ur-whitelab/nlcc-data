74df7080a861ff2604a8cfb6d15e63244fd2595a
Sam Cox <samantha.wright822@gmail.com>
2021-09-13 16:00:24 -0400

    added square root example

--- data/electric_side_chains/electric_side_chains.py
+++
@@ -1,6 +0,0 @@
-arginine = "R"
-aspartic_acid = "D"
-alanine = "A"
-
-
-result = True if electric_side_chains(arginine)=="positive" and electric_side_chains(aspartic_acid)=="negative" and electric_side_chains(alanine)=="no e charge" and electric_side_chains("X")=="error" else False
---
+++ data/square_root/square_root.py
@@ -0,0 +1,4 @@
+root = 7
+n = 9
+
+result = True if square_root(root, n)==2.645751311 else False
