cad3b22390d65676209aee1411d702edc27f9404
Mehrad Ansari <mehrad.ans@gmail.com>
2021-09-13 17:36:56 -0400

    added ohe

---
+++ data/ohe/ohe.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+dog_breeds = ['German Shepherd', 'Husky',
+              'Bulldog', 'Dobermann', 'Great Dane', 'Husky', 'Bulldog', 'German Shepherd', 'Great Dane', 'German Shepherd']
+
+unique_list = sorted(list(set(dog_breeds)))
+n = len(dog_breeds)
+m = len(unique_list)
+ohe_array = np.zeros((n, m))
+j = 0
+for i in range(n):
+        print(j)
+        ohe_array[j, unique_list.index(dog_breeds[i])] = 1.
+        j += 1
+
+result = True if np.isclose(ohe(dog_breeds), ohe_array) else False
