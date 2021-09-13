22ba1e8f8f43aed0bbb4615b1c9a2fe5c1ee1ec6
Sam Cox <samantha.wright822@gmail.com>
2021-09-13 15:47:25 -0400

    fixed quotations

--- data/electric_side_chains/electric_side_chains.py
+++ data/electric_side_chains/electric_side_chains.py
@@ -3,4 +3,4 @@ aspartic_acid = "D"
 alanine = "A"
 
 
-result = True if electric_side_chains(arginine)=="positive" and electric_side_chains(aspartic_acid)=="negative" and electric_side_chains(alanine)=="no e charge" and electric_side_chains(X)=="error" else False
+result = True if electric_side_chains(arginine)=="positive" and electric_side_chains(aspartic_acid)=="negative" and electric_side_chains(alanine)=="no e charge" and electric_side_chains("X")=="error" else False
