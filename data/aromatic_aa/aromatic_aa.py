1719d5cbd1c83316f357858b022c01a830e26361
Sam Cox <samantha.wright822@gmail.com>
2021-09-13 14:32:29 -0400

    added error option for function

--- data/aromatic_aa/aromatic_aa.py
+++ data/aromatic_aa/aromatic_aa.py
@@ -2,6 +2,6 @@ phenylalanine = "F"
 leucine = "L"
 
 
-result = True if aromatic_aa(phenylalanine)=="aromatic" and aromatic_aa(leucine) =="non-aromatic" else False
+result = True if aromatic_aa(phenylalanine)=="aromatic" and aromatic_aa(leucine) =="non-aromatic" and aromatic_aa("X") =="error" else False
 
 
