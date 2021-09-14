3fb3013124e7dfe58cc0d7850fd481b829a253e6
Sam Cox <samantha.wright822@gmail.com>
2021-09-14 12:49:01 -0400

    changed input type and context

--- data/h_donor_acceptor/h_donor_acceptor.py
+++
@@ -1,6 +0,0 @@
-arginine = "R"
-glutamic_acid = "Z"
-serine = "S"
-
-
-result = True if h_donor_acceptor(arginine) == "donor" and h_donor_acceptor(glutamic_acid) == "acceptor" and h_donor_acceptor(serine) == "both" and h_donor_acceptor(X) == "error" else False
