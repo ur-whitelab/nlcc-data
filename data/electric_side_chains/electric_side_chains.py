cf197c226d3ada1dedfd40e7817a233c27b77661
Sam Cox <samantha.wright822@gmail.com>
2021-09-14 12:17:52 -0400

    changed context, input string, and numbered steps in prompt for electrically_charged_side_chains

--- data/electric_side_chains/electric_side_chains.py
+++ data/electric_side_chains/electric_side_chains.py
@@ -1,6 +1,2 @@
-arginine = "R"
-aspartic_acid = "D"
-alanine = "A"
 
-
-result = True if electric_side_chains(arginine)=="positive" and electric_side_chains(aspartic_acid)=="negative" and electric_side_chains(alanine)=="no e charge" and electric_side_chains("X")=="error" else False
+result = True if electric_side_chains("R")=="positive" and electric_side_chains(a"D")=="negative" and electric_side_chains("A")=="no e charge" and electric_side_chains("X")=="error" else False
