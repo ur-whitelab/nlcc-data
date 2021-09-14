3049b5db4a1dbd2955641ef45e30780b66493e0b
Sam Cox <samantha.wright822@gmail.com>
2021-09-14 13:14:15 -0400

    changed input to sequence

--- data/aromatic_aa/aromatic_aa.py
+++ data/aromatic_aa/aromatic_aa.py
@@ -1,7 +1,3 @@
-phenylalanine = "F"
-leucine = "L"
 
-
-result = True if aromatic_aa(phenylalanine)=="aromatic" and aromatic_aa(leucine) =="non-aromatic" and aromatic_aa("X") =="error" else False
-
+result = True if aromatic_aa("FLRYSCV")==2 else False
 
