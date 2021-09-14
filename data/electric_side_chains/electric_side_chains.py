7d855a5b685e0bc223c5508fe0aa4ec2de1099e4
Sam Cox <samantha.wright822@gmail.com>
2021-09-14 12:19:07 -0400

    changed context, input string, and numbered steps in prompt for electrically_charged_side_chains

--- data/electric_side_chains/electric_side_chains.py
+++ data/electric_side_chains/electric_side_chains.py
@@ -1,2 +1,2 @@
 
-result = True if electric_side_chains("R")=="positive" and electric_side_chains(a"D")=="negative" and electric_side_chains("A")=="no e charge" and electric_side_chains("X")=="error" else False
+result = True if electric_side_chains("R")=="positive" and electric_side_chains("D")=="negative" and electric_side_chains("A")=="no e charge" and electric_side_chains("X")=="error" else False
