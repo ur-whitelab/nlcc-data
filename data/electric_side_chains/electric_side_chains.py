4ad9ff39506a6139f04031a0c5ce754104f673d9
Sam Cox <samantha.wright822@gmail.com>
2021-09-14 13:04:48 -0400

    changed input type, output, and context

--- data/electric_side_chains/electric_side_chains.py
+++
@@ -1,2 +0,0 @@
-
-result = True if electric_side_chains("R")=="positive" and electric_side_chains("D")=="negative" and electric_side_chains("A")=="no e charge" and electric_side_chains("X")=="error" else False
