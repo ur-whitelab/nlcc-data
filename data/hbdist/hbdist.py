7328feb9063f7ac84cd57ca727d84dd26dc89a8b
Willmor J Pena ccoa <wpc252@log-2.nyu.cluster>
2021-09-19 17:44:52 -0400

    compute hbond length for hbonds in pdb

---
+++ data/hbdist/hbdist.py
@@ -0,0 +1,17 @@
+import mdtraj as md
+import os 
+import random
+#  _FILE_DIR_ = '.'
+top_path = os.path.join(_FILE_DIR_,'top.pdb')
+system = md.load_pdb(top_path)
+h_bonds = md.baker_hubbard(system, periodic=False)
+da_dist = md.compute_distances(system, h_bonds[:,[0,2]], periodic=False)
+num = len(h_bonds)
+# assert
+hb_dist = hbdist(top_path)
+idx_1 = random.randint(0,num)
+check1 = hb_dist[idx_1] == da_dist[idx_1]
+idx_2 = random.randint(0,num)
+check2 = hb_dist[idx_2] == da_dist[idx_2]
+
+result = True if check1 and check2 else False
