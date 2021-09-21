d91c7b95ce9742580d532cd6c7dbc6ec6ab05e7c
Mehrad Ansari <mehrad.ans@gmail.com>
2021-09-21 09:20:13 -0400

    fixed func name

--- data/kld_loss/kld.py
+++ data/kld_loss/kld.py
@@ -4,7 +4,7 @@ np.random.seed(0)
 M = 100
 targets = np.random.rand(M)
 predictions = targets * 0.9
-kle_loss = -np.sum(targets*np.log(
+kld_loss = -np.sum(targets*np.log(
     np.divide(predictions, targets) + 1e-10))
 
-result = True if np.isclose(kle(targets, predictions), kle_loss) else False
+result = True if np.isclose(kld(targets, predictions), kld_loss) else False
