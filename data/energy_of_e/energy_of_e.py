134caa1ff82d540f40538e11bdc42e8cc1d70e8c
Sam Cox <samantha.wright822@gmail.com>
2021-09-21 13:55:58 -0400

    fixed typo

--- data/energy_of_e/energy_of_e.py
+++ data/energy_of_e/energy_of_e.py
@@ -5,6 +5,5 @@ n = 3
 energy_true = (-Rh/(n**2))
 energy_nlcc = energy_of_e(n)
 
-
-#result = math.isclose(energy_true, energy_nlcc, rel_tol =1e-3)
+result = math.isclose(energy_true, energy_nlcc, rel_tol =1e-3)
 
