306964813fb7f63a0c6a176bd70fc536e2f60f9e
Sam Cox <samantha.wright822@gmail.com>
2021-09-21 12:23:55 -0400

    changed units and tolerance

--- data/energy_of_e/energy_of_e.py
+++ data/energy_of_e/energy_of_e.py
@@ -1,9 +1,10 @@
 
 import math
-Rh = 2.18e-18
+Rh = 13.60648962
 n = 3
 energy_true = (-Rh/(n**2))
 energy_nlcc = energy_of_e(n)
 
-result = math.isclose(energy_true, energy_nlcc, rel_tol =1e-5)
+
+#result = math.isclose(energy_true, energy_nlcc, rel_tol =1e-3)
 
