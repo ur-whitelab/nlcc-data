a800272c1c0da4becf719519999afbbf67b6fda1
Navneeth Gokul <navneethgokul1991@gmail.com>
2021-09-22 10:02:14 -0400

    Added fourier folder. Added code for 1d_fourier. (#83)
    * Added fourier folder. Added code for 1d_fourier.
    
    * Fix 1d_fourier.py
    
    * changed 1d_fourier to fourier_1d
    
    * Changed context to numpy in yml. Using numpy instead of scipy.

---
+++ data/fourier/fourier_1d.py
@@ -0,0 +1,10 @@
+import numpy as np
+from numpy.fft import fft
+
+x = np.array([1.0, 2.0, 1.0, -1.0, 1.5])
+
+true_y = fft(x)
+
+test_y = fourier_1d(x)
+
+result = True if numpy.allclose(true_y,test_y) else False
