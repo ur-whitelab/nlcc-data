bb29a7aafc97400eb36556d34bf172896c3d3e7d
Mehrad Ansari <51170839+mehradans92@users.noreply.github.com>
2021-09-22 10:01:52 -0400

    added ohe (#92)
    * added ohe
    
    * removed extra print
    
    * added all assertion

---
+++ data/ohe/ohe.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+dog_breeds = ['German Shepherd', 'Husky',
+              'Bulldog', 'Dobermann', 'Great Dane', 'Husky', 'Bulldog', 'German Shepherd', 'Great Dane', 'German Shepherd']
+
+unique_list = sorted(list(set(dog_breeds)))
+n = len(dog_breeds)
+m = len(unique_list)
+ohe_array = np.zeros((n, m))
+j = 0
+for i in range(n):
+        ohe_array[j, unique_list.index(dog_breeds[i])] = 1.
+        j += 1
+
+result = True if np.isclose(ohe(dog_breeds), ohe_array).all() else False
