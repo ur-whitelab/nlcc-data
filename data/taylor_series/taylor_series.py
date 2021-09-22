84464fafd933243dfa2f4adaded938a9982cdbf6
Mehrad Ansari <51170839+mehradans92@users.noreply.github.com>
2021-09-22 10:02:43 -0400

    added taylor series (#73)
    * added taylor series
    
    * updated changes
    
    * updated assertion

---
+++ data/taylor_series/taylor_series.py
@@ -0,0 +1,17 @@
+import numpy as np
+import math
+
+def func_cos(x, n):
+    cos_approx = 0
+    for i in range(n):
+        coef = (-1)**i
+        num = x**(2*i)
+        denom = math.factorial(2*i)
+        cos_approx += (coef) * ((num)/(denom))
+    return cos_approx
+
+x = math.radians(45)
+n = 8
+
+result = True if np.isclose(taylor_series(
+    np.cos,x,n), func_cos(x,n)) else False
