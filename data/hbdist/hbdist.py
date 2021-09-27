93f8106563b56a4c97b022c81dc0eae6eac91265
Glen Hocky <hockyg@nyu.edu>
2021-09-27 10:30:47 -0400

    changed eval critereon to use shortest hydrogen bonded length. note, nlcc gives pretty good code just minor error

--- data/hbdist/hbdist.py
+++ data/hbdist/hbdist.py
@@ -6,12 +6,10 @@ top_path = os.path.join(_FILE_DIR_,'top.pdb')
 system = md.load_pdb(top_path)
 h_bonds = md.baker_hubbard(system, periodic=False)
 da_dist = md.compute_distances(system, h_bonds[:,[0,2]], periodic=False)
-num = len(h_bonds)
-# assert
+num = len(da_dist[0])
 hb_dist = hbond_distance(top_path)
-idx_1 = 1 # random.randint(0,num)
-check1 = abs(hb_dist[idx_1] - da_dist[idx_1])
-#idx_2 = random.randint(0,num)
-#check2 = abs(hb_dist[idx_2] - da_dist[idx_2])
+
+#compare shorted hbond length
+check1 = abs(hb_dist[np.argsort(hb_dist)[0]] - da_dist[np.argsort(da_dist)[0]])
 tol = 0.01
 result = True if check1 <= tol else False
