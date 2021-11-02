d0bf7977d4e0938bd357dd141ac0e0205167e0db
Andrew White <white.d.andrew@gmail.com>
2021-11-02 11:39:36 -0400

    Removed determinant

--- data/determinant/determinant.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-np.random.seed(0)
-M = 10
-a = np.random.randn(M, M)
-test_determinant = np.linalg.det(a)
-
-result = True if np.isclose(
-    determinant(a), test_determinant) else False
