46da52b01dcd81a3fd80aaadcc2bd839e2c6383f
Andrew White <white.d.andrew@gmail.com>
2021-11-16 10:58:40 -0500

    Fixed substruct, taylor, vapor

--- data/substructure/substructure.py
+++ data/substructure/substructure.py
@@ -1,13 +1,17 @@
 import numpy as np
+import os
 # Taken from rdkit Docs
 from rdkit import RDConfig, Chem
 from rdkit.Chem import rdSubstructLibrary
 library = rdSubstructLibrary.SubstructLibrary()
-for mol in Chem.SDMolSupplier(os.path.join(RDConfig.RDDataDir, 
-                              'NCI', 'first_200.props.sdf')):
+llibrary = []
+for mol in Chem.SDMolSupplier(os.path.join(RDConfig.RDDataDir,
+                                           'NCI', 'first_200.props.sdf')):
     idx = library.AddMol(mol)
+    llibrary.append(mol)
 smiles = 'CCCCOC'
 core = Chem.MolFromSmiles(smiles)
 indices = library.GetMatches(core)
 
-result = True if np.isclose(substructure(smiles, library), len(indices)) else False
+result = True if np.isclose(len(substructure(
+    smiles, llibrary)), len(indices)) else False
