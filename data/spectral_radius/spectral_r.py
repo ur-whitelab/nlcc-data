42a24e8476a198b9e632f981eafa0707d1e8600c
Mehrad Ansari <mehrad.ans@gmail.com>
2021-11-16 15:21:39 -0500

    revised spectral_r formula

--- data/spectral_radius/spectral_r.py
+++ data/spectral_radius/spectral_r.py
@@ -2,11 +2,8 @@ import numpy as np
 
 np.random.seed(0)
 M = 100
-# (k-> inf)
-k = 50
 a = np.random.normal(loc=10, scale=3, size=(M,M))
-matrix = np.linalg.matrix_power(a, k)
-f_norm = np.linalg.norm(matrix, 'fro')
-s_r = f_norm**(1.0/k)
+eig = np.linalg.eigvals(a)
+s_r = np.amax(np.absolute(eig))
 
 result = True if np.isclose(spectral_r(a), s_r) else False
