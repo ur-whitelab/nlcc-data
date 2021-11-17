8b9b2d44dfd8a68e0824edd529e70905dcd11ad9
Glen Hocky <hockyg@nyu.edu>
2021-11-17 10:17:27 -0500

    updated pyscf requirements and test, as well as simplified prompt

--- data/pyscf_energy/pyscf_energy.py
+++ data/pyscf_energy/pyscf_energy.py
@@ -1,15 +1,16 @@
 atom_coordinates = "H 0 0 0; F 0 0 1"
 basis = "sto-3g"
 
-def calc_molecule_energy(atom_coordinates,basis):
-    import pyscf
-    from pyscf import gto, scf
+#so that it doesn't fail b/c not importing these in prompt:
+from pyscf import gto, scf
+
+def calc_molecule_energy_test(atom_coordinates,basis):
     mol = gto.M(atom=atom_coordinates,basis=basis)
     mf = scf.RHF(mol)
     return mf.kernel()
 
-my_energy = calc_molecule_energy(atom_coordinates,basis)
-nlcc_energy = calc_molecule_energy_nlcc(atom_coordinates,basis)
+my_energy = calc_molecule_energy_test(atom_coordinates,basis)
+nlcc_energy = calc_molecule_energy(atom_coordinates,basis)
 
 print("My result:", my_energy)
 print("nlcc result:", nlcc_energy)
