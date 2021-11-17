7172a6e87f0e98b683a6dd910eda10d23941b32a
Glen Hocky <hockyg@nyu.edu>
2021-11-17 10:06:29 -0500

    changed ideal gas example to be more challenging by not providing R. Made comparison less precise because sometimes codex produces an R to more decimal places

--- data/ideal_gas/ideal_gas.py
+++ data/ideal_gas/ideal_gas.py
@@ -3,14 +3,16 @@ import numpy as np
 # input
 V = 20.0    # volume (in L)
 n = 10.0    # in moles
-R = 0.0821  # in L.atm/mol.K
+R = 0.08206  # in L.atm/mol.K
 T = 350     # in K 
 
 P = n*R*T/V
-print("Pressure =", P)
+#print("Pressure =", P)
 
+codex_pressure = ideal_gas_pressure(n,V,T)
+#print("Codex pressure =", codex_pressure)
 # check 
-if np.isclose(P, ideal_gas_equ(n,R,T,V)) == True:
+if np.isclose(P, codex_pressure, rtol=0.01) == True:
     result = True 
 else:
     result = False
