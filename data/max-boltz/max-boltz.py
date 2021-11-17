0095b1c1d85ec5d35431d370031b9b72088b3960
Glen Hocky <hockyg@nyu.edu>
2021-11-17 10:47:37 -0500

    maxwell boltzmann root mean squared velocity code sometimes passes now, main issue was units before, and also not giving the mass but giving a molecule name instead

--- data/max-boltz/max-boltz.py
+++ data/max-boltz/max-boltz.py
@@ -1,29 +1,20 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 # input params
 M = 32e-3           # for O2, kg/mol
 R = 8.314           # J/mol.K
-T = np.array([100, 200, 300, 400, 500, 600])
+T = 300
 
 c_rms = np.sqrt(3.0*R*T/M)
-print("rms speeds :", c_rms)
+#print("rms speeds :", c_rms)
 
-c_rms_codex = max_boltz_speed(T)
-print("rms speeds from codex :", c_rms_codex)
+M_in_kg = M/6.022e23
+
+c_rms_codex = max_boltz_rms_speed(T,M_in_kg)
+#print("rms speeds from codex :", c_rms_codex)
 
 # check
-if np.all(abs(c_rms - c_rms_codex) <= 1e-2):
+if np.isclose(c_rms,c_rms_codex,rtol=0.01):
     result = True 
 else:
     result = False
-
-
-"""
-plt.figure()
-plt.grid()
-plt.xlabel('Temperature (K)')
-plt.ylabel('RMS speed (m/s)')
-plt.plot(T, c_rms, 'o-', lw=2.0)
-plt.show()
-"""
