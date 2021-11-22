bc10cc265e3dc898f202bdd91d45f17bb038dffb
Glen Hocky <hockyg@nyu.edu>
2021-11-21 21:49:35 -0500

    output sasa code was good, changed acceptance critereon and changed to per residue to make it specific what asking

--- data/sasa/sasa.py
+++ data/sasa/sasa.py
@@ -8,7 +8,7 @@ def compute_sasa_mdtraj(pdb_file):
     import mdtraj as md
     pdb = md.load(pdb_file)
     
-    sasa = md.shrake_rupley(pdb)
+    sasa = md.shrake_rupley(pdb,mode='residue')
     return sasa
 
 my_sasa = compute_sasa_mdtraj(pdb_file)
@@ -17,4 +17,4 @@ nlcc_sasa = protein_surface_area(pdb_file)
 #print("My result:", my_sasa)
 #print("nlcc result", nlcc_sasa)
 
-result = True if np.abs( my_sasa - nlcc_sasa )<0.1 else False
+result = True if np.abs( my_sasa.mean() - nlcc_sasa.mean() )<0.1 else False
