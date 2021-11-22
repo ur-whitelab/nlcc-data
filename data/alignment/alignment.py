8e59ce8b8b2537c8381a5d2e321cc5f9132b736f
Glen Hocky <hockyg@nyu.edu>
2021-11-21 22:05:40 -0500

    slightly changed prompt and eval criterea. need a two frame pdb

--- data/alignment/alignment.py
+++ data/alignment/alignment.py
@@ -1,23 +1,22 @@
 import numpy as np
 import sys
 import os
+import mdtraj as md
 
 pdb_file = os.path.join(_FILE_DIR_,"1VII.pdb")
+trj = md.load(pdb_file)
 
-def alingment(pdb_file):
-    import mdtraj as md
-    pdb = md.load(pdb_file)
-    alignment = pdb.superpose(pdb,0)
-    return alignment
-    rmsd=md.rmsd(pdb,pdb, 0,alignment,precentered=True)
+def my_rmsd_aligned(trj):
+    alignment = trj.superpose(trj,0)
+    rmsd=md.rmsd(alignment, alignment, 0, precentered=True)
+    return rmsd
 
+my_rmsd = my_rmsd_aligned(trj)
+nlcc_trj = get_aligned_trj(trj, 0)
+nlcc_rmsd = md.rmsd(nlcc_trj, nlcc_trj, 0, precentered=True)
 
-
-my_rmsd = alingment(pdb_file)
-nlcc_rmsd=protein_alignment(pdb_file)
-
-#print("My result:", my_rmsd)
-#print("nlcc result", nlcc_rmsd)
+print("My result:", my_rmsd)
+print("nlcc result", nlcc_rmsd)
 
 result = True if np.abs( my_rmsd - nlcc_rmsd )<0.1 else False
 
