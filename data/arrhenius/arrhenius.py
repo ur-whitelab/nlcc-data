0524cbb9ae0b2f5e9e5b1b982b018930f9925545
Glen Hocky <hockyg@nyu.edu>
2021-11-30 12:10:05 -0500

    tweaked prompt, now arrhenius results from codex are only off by a minus sign, which shows an interesting failure mode

--- data/arrhenius/arrhenius.py
+++ data/arrhenius/arrhenius.py
@@ -7,9 +7,11 @@ R = 8.314     # J/mol.K
 Ea = 108000   # J/mol
 k2 = 1e-3     # M-1 s-1 
 
-k1 = k2/(np.exp((Ea/R) * (1/T1 - 1/T2)))
+k1 = k2*np.exp((-Ea/R) * (1/T1 - 1/T2))
+k1_codex = arrhenius(k2,T2,T1,Ea)
+#print(k1,k1_codex)
 
-if np.isclose(k1, arrhenius(k2,T2,T1,Ea,R)) == True:
+if np.isclose(k1,k1_codex) == True:
     result = True
 else:
     result = False
