4703f968c784d99c42b0d878e05021dd9ebb4d57
Glen Hocky <hockyg@nyu.edu>
2021-11-30 17:08:27 -0500

    tweak langevin, now gives a somewhat reasonable answer some fraction of the time

--- data/langevin/LD.py
+++ data/langevin/LD.py
@@ -1,11 +1,17 @@
 import numpy as np
-def PE(q):
-    return (q**2-1.0)**2
-def F(q):
-    return -4.0 * q * (q**2-1.0)
+import pylab as plt
+#def PE(q):
+#    return (q**2-1.0)**2
+#def F(q):
+#    return -4.0 * q * (q**2-1.0)
+def F(q,k=2):
+    return -k*q
 
-q_trj,p_trj = run_langevin_dynamics(1.0,0.0,1.0,1.0,2.0,PE,F,10000,dt=0.001,k_B=1.0)
-p_trj = np.array(p_trj)[len(p_trj)//2:]
-second_moment = np.mean(p_trj**2)
-ideal = 1.0 * 1.0 * 2.0
-result = True if np.abs(second_moment-ideal)/ideal < 0.1 else False
+x_traj, v_traj = run_langevin_dynamics(0.1,-0.1,0.1,F,100000)
+plt.plot(x_traj)
+plt.show()
+v_traj_half = np.array(v_traj)[len(v_traj)//2:]
+msv = v_traj_half.var()
+ideal_msv = 1 #kT/m
+
+result = True if np.abs(msv-ideal_msv)/ideal_msv < 0.2 and np.abs(np.mean(x_traj[len(x_traj)//2:]))<0.1 else False
