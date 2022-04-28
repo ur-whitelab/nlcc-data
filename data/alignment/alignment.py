9ecfa9d10cb1594c79fd1bbec4a6970e55ab34d7
Andrew White <white.d.andrew@gmail.com>
2022-04-28 16:36:12 -0400

    Added inserts into all data

--- data/alignment/alignment.py
+++ data/alignment/alignment.py
@@ -3,26 +3,28 @@ import sys
 import os
 import mdtraj as md
 
-pdb_file = os.path.join(_FILE_DIR_,"1VII_twoframe_shifted.pdb")
+pdb_file = os.path.join(_FILE_DIR_, "1VII_twoframe_shifted.pdb")
 trj = md.load(pdb_file)
 
+
 def myrmsd(trj, frame):
-    xyz = trj.xyz 
+    xyz = trj.xyz
     xyz0 = trj.xyz[frame]
     dx = xyz-xyz0
     dx2 = (dx*dx).sum(axis=2)
     rmsd = np.sqrt(dx2.mean(axis=1))
     return rmsd
 
+
 #print("Starting rmsd:", myrmsd(trj, 0))
-aligned_trj = trj.superpose(trj,0)
+aligned_trj = trj.superpose(trj, 0)
 
 my_rmsd = myrmsd(aligned_trj, 0)
 #print("Aligned rmsd:", my_rmsd)
 
-nlcc_trj = get_aligned_trj(trj, 0)
+nlcc_trj = align_traj(trj, 0)
 nlcc_rmsd = myrmsd(nlcc_trj, 0)
 
 #print("nlcc rmsd", nlcc_rmsd)
 
-result = True if np.all(np.abs( my_rmsd - nlcc_rmsd ))<0.01 else False
+result = True if np.all(np.abs(my_rmsd - nlcc_rmsd)) < 0.01 else False
--- data/trig/cos.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-x = np.array((0., 30., 45., 60., 90.))
-
-true_y = np.cos(x)
-
-pred_y = f_cos(x)
-
-result = True if np.allclose(true_y, pred_y) else False
