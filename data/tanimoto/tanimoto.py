12ccc89703f9efc63f9a950f522188b14f6e3fcf
Andrew White <white.d.andrew@gmail.com>
2022-04-29 12:06:34 -0400

    Set-up pypi env to June 2021

--- data/tanimoto/tanimoto.py
+++ data/tanimoto/tanimoto.py
@@ -6,8 +6,8 @@ import itertools
 
 
 def ref_tan_matrix(slist):
-    fp = [AllChem.GetMorganFingerprint(
-        Chem.MolFromSmiles(s), 2) for s in slist]
+    fp = [AllChem.GetMorganFingerprintAsBitVect(
+        Chem.MolFromSmiles(s), 2, nBits=256) for s in slist]
     ts = list(
         TanimotoSimilarity(x, y) for x, y in itertools.product(fp, repeat=2)
     )
@@ -16,10 +16,6 @@ def ref_tan_matrix(slist):
 
 mols = ['C1C=C(C=NC=C(C(F)(F)F)CCCCCC)C=C1C=CC=CC',
         'CC=C=C(C(=O)N)', 'C1([O-])C=CC=C1NC=CC=C']
-print(tanimoto_matrix(mols))
-print(ref_tan_matrix(
-    mols))
-
 # turned down tolerance because CODEX wants to compute
 # fingerprint with slightly different implementation (can't tell diff?)
 result = True if np.allclose(ref_tan_matrix(
