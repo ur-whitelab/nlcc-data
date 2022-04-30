c7b5fb85ee58a26ca021f6c1e4705f0c00444c28
Andrew White <white.d.andrew@gmail.com>
2022-04-29 20:18:15 -0400

    Removed foreign language prompts

--- data/rmse-ch/rmse-ch.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-np.random.seed(0)
-M = 100
-predictions = np.random.normal(loc=10, scale=3, size=(M,))
-targets = np.random.normal(loc=9, scale=2, size=(M,))
-
-rmse = np.sqrt(((predictions - targets) ** 2).mean())
-result = True if np.isclose(rmse, 3.61350) else False
